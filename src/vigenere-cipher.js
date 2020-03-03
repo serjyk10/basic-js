802c85e9f60c847c3bce32208848ef508e6860c3
Sergey Borisevich <borisevich.serjyk@gmail.com>
2020-03-03 14:23:35 +0300

    feat: vigenere-cipher is complited

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,40 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(direct = true) {
+        this.direct = direct;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(message, key) {
+        if (!message || !key) throw new Error();
+        let keyArr = key.toUpperCase().split('');
+
+        let result = message.split('').map(letter => {
+            let codeOfLetter = letter.toUpperCase().charCodeAt(0) - 65;
+            if (codeOfLetter < 0 || codeOfLetter > 25) return letter;
+
+            let codeOfKey = keyArr.shift().charCodeAt(0) - 65;
+            keyArr.push(String.fromCharCode(codeOfKey + 65));
+
+            return String.fromCharCode((codeOfLetter + codeOfKey) % 26 + 65);
+        });
+
+        return this.direct ? result.join('') : result.reverse().join('');
+    }  
+
+    decrypt(encryptedMessage, key) {
+        if (!encryptedMessage || !key) throw new Error();
+        let keyArr = key.toUpperCase().split('');
+
+        let result = encryptedMessage.split('').map(letter => {
+            let codeOfLetter = letter.toUpperCase().charCodeAt(0) - 65;
+            if (codeOfLetter < 0 || codeOfLetter > 25) return letter;
+
+            let codeOfKey = keyArr.shift().charCodeAt(0) - 65;
+            keyArr.push(String.fromCharCode(codeOfKey + 65));
+
+            return String.fromCharCode((codeOfLetter + 26 - codeOfKey) % 26 + 65);
+        });
+
+        return this.direct ? result.join('') : result.reverse().join('');
     }
 }
-
 module.exports = VigenereCipheringMachine;
